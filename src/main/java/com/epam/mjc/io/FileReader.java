6c7659daad4dd4256ab14408edca4aef32ae3e51
limbxbx <ilya.msd2003@gmail.com>
2023-03-09 18:10:32 +0200

    Task complete

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,9 +3,47 @@ package com.epam.mjc.io;
 import java.io.File;
 
 
-public class FileReader {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+public class FileReader {
+    public static Profile getDataFromFile(File file){
+        Path filePath = file.toPath();
+        String fileContent = null;
+        try {
+            fileContent = Files.readString(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert fileContent != null;
+        Map<String, String> keyValuePairs = parseStringForKeyValuePairs(fileContent);
+
+        String name = keyValuePairs.get("Name");
+        int age = Integer.parseInt(keyValuePairs.get("Age"));
+        String email = keyValuePairs.get("Email");
+        String phone = keyValuePairs.get("Phone");
+
+        return new Profile(name, age, email, phone);
+    }
+
+    private static Map<String, String> parseStringForKeyValuePairs(String string) {
+        Map<String, String> keyValuePairs = new HashMap<>();
+
+        String[] lines = string.split("\n");
+        for (String line : lines) {
+            String[] parts = line.split(": ");
+            if (parts.length == 2) {
+                String key = parts[0].trim();
+                String value = parts[1].trim();
+                keyValuePairs.put(key, value);
+            }
+        }
+
+        return keyValuePairs;
     }
 }
--- src/main/java/com/epam/mjc/io/Profile.java
+++ src/main/java/com/epam/mjc/io/Profile.java
@@ -19,6 +19,9 @@ public class Profile {
         this.phone = phone;
     }
 
+    public Profile(String name, int age, String email, String phone) {
+    }
+
     public String getName() {
         return name;
     }
